29d07621c62cb8e0988f26250ac1e95a5502cd82
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-03-18 09:23:56 -0400

    a

--- vue-api-project/src/components/apiChart.vue
+++ vue-api-project/src/components/apiChart.vue
@@ -6,6 +6,7 @@
 
 <script>
 import { Bar } from 'vue-chartjs'
+import { ref } from 'vue'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
@@ -18,14 +19,26 @@ export default {
     chartData: null,
   }),
   async mounted () {
-    this.loaded = false
-
+    const loaded = ref();
+    const death = ref([]);
     try {
       const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json');
       const data = await response.json();
-      this.chartData = data;
-
-      this.loaded = true
+      const chartData = data;
+      death.map((dead => dead.value = {
+      chartData: {
+        labels: [chartData[1].leading_cause,],
+        apiData: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [chartData[1].deaths]
+          }
+        ]
+      }
+    }))
+    console.log(death)
+    loaded.value == true
     } catch (e) {
       console.error(e)
     }
