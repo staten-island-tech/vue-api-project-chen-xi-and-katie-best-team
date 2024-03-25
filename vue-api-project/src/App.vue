ca7e750b37194d35ec4c52b01a49bc330ea3eb43
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2024-03-25 09:22:50 -0400

    a

--- vue-api-project/src/App.vue
+++ vue-api-project/src/App.vue
@@ -6,10 +6,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
       <nav>
-        <h1>Leading Causes of Deaths</h1>
         <RouterLink to="/">Bar Chart</RouterLink>
         <RouterLink to="/pie">Pie Chart</RouterLink>
         <RouterLink to="/scatter">Scatter Chart</RouterLink>
+        <h1>Leading Causes of Deaths</h1>
       </nav>
   </header>
   <RouterView/>
